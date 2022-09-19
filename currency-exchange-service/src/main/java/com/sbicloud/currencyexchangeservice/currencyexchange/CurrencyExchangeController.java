a78b88e08c0e6ebd06d8b012c8e21d01344aed46
ershad <ershad@gmail.com>
2022-09-19 12:56:27 +0600

    polishing

---
+++ currency-exchange-service/src/main/java/com/sbicloud/currencyexchangeservice/currencyexchange/CurrencyExchangeController.java
@@ -0,0 +1,7 @@
+package com.sbicloud.currencyexchangeservice.currencyexchange;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyExchangeController {
+}
---
+++ limits-service/src/main/java/com/sbicloud/limitsservice/limits/Limits.java
@@ -0,0 +1,15 @@
+package com.sbicloud.limitsservice.limits;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Limits {
+    private int minimum;
+    private int maximum;
+
+
+}
