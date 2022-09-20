b10443ba6face2cb44932989f3d20e66ddebd3bf
ershad <ershad@gmail.com>
2022-09-20 11:49:28 +0600

    polishing

---
+++ currency-conversion-service/src/main/java/com/sbicloud/currencyconversionservice/currencyconversion/CurrencyConversion.java
@@ -0,0 +1,13 @@
+package com.sbicloud.currencyconversionservice.currencyconversion;
+
+import java.math.BigDecimal;
+
+public class CurrencyConversion {
+    Long UUID;
+    private String from;
+    private String to;
+    private BigDecimal conversationMultiple;
+    private BigDecimal quantity;
+    private BigDecimal totalCalculatedAmount;
+    private String environment;
+}
---
+++ currency-conversion-service/src/main/java/com/sbicloud/currencyconversionservice/currencyconversion/CurrencyConversionController.java
@@ -0,0 +1,20 @@
+package com.sbicloud.currencyconversionservice.currencyconversion;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyConversionController {
+    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversion(
+            @PathVariable String from,
+            @PathVariable String to,
+            @PathVariable BigDecimal quantity
+    ) {
+
+        return new CurrencyConversion();
+    }
+}
